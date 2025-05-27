7e45dbd53acc8702fcf33da2867bc801a13866c5
antc500 <anton.ch500@gmail.com>
2025-05-27 09:08:42 +0000

    create .js

---
+++ script.js
@@ -0,0 +1,21 @@
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-container').innerHTML = data;
+      initMobileMenu();
+    });
+
+  function initMobileMenu() {
+    const hamburger = document.querySelector('.hamburger');
+    if (hamburger) {
+      hamburger.addEventListener('click', () => {
+        document.getElementById('mobileMenu').classList.toggle('active');
+      });
+    }
+  }
+
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    });
